59310d1b042e19ddac13dc94bf63dd0e514efe7b
mishkathbk <mishkathbk@gmail.com>
2023-12-04 14:17:35 +0530

    navbar logo transition

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,13 +7,13 @@ const Footer = () => {
     <div className=" w-full h-28 bg-blue-500 mt-10 ">
         <div className='flex flex-row justify-center mt-4'>
             <Link href="https://www.instagram.com/m.ishqu?igshid=MmIzYWVlNDQ5Yg%3D%3D">
-             <Image className=" mt-5 ml-4" src="/images/instagram.png" width={30} height={30} alt=""/>
+             <Image className="origin-center hover:-skew-y-6 mt-5 ml-4" src="/images/instagram.png" width={30} height={30} alt=""/>
              </Link>
              <Link href="https://www.linkedin.com/in/mishkath-bk">
-             <Image className=" mt-5 ml-4" src="/images/linkedin.png" width={30} height={30} alt=""/>
+             <Image className="origin-center hover:-skew-y-6 mt-5 ml-4" src="/images/linkedin.png" width={30} height={30} alt=""/>
              </Link>
              <Link href="https://www.github.com/mishkathbk">
-             <Image className=" mt-5 ml-4" src="/images/github.svg" width={30} height={30} alt=""/>
+             <Image className="origin-center hover:-skew-y-6 mt-5 ml-4" src="/images/github.svg" width={30} height={30} alt=""/>
              </Link>
         </div>
     </div>
