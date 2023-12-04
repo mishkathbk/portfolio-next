b5e6d29270f5352ed3444e4c8a505e9b272b39fc
mishkathbk <mishkathbk@gmail.com>
2023-12-04 14:11:03 +0530

    navbar transition

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -3,17 +3,15 @@ import Link from 'next/link'
 import ExampleDoc from '../../../public/images/graphic-design.png'
 
 const Button = () => {
-  const butt=()=>{
+  const butt = () => {
     window.location.href = "https://drive.google.com/file/d/1rDRZlrEa_v3ElFtIH8DsyNSeQ7BpmueK/view?usp=sharing"
 
   }
 
   return (
     <div className='  flex items-center justify-center  mt-14' id="resume">
-       <button  className="text-center  text-white bg-blue-900 rounded-lg p-2 hover:text-blue-950 font-semibold hover:bg-white transition duration-700 " onClick={butt} >Download Resume</button>
-          
-
-        </div>
+      <button className="text-center  text-white bg-blue-900 rounded-lg p-2 hover:text-blue-950 font-semibold hover:bg-white transition duration-700 " onClick={butt} >Download Resume</button>
+    </div>
   )
 }
 
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,11 +10,11 @@ const Navbar = () => {
     <div className='block sm:sticky top-0 z-10'>
       <div className="flex flex-row items-center justify-center  bg-white">
           <ul className='hidden sm:flex sm:flex-row font-opensans sm:gap-10 md:gap-24 h-14 sm:text-1xl  m-auto justify-between '>
-              <li className='flex items-center justify-center   hover:text-blue-950 hover:scale-110 hover:font-semibold'><Link href="#intro">Home</Link></li>
-              <li className='flex items-center justify-center   hover:text-blue-950 hover:scale-110 hover:font-semibold'><Link href="#about">About</Link></li>
-              <li className='flex items-center justify-center   hover:text-blue-950 hover:scale-110 hover:font-semibold'><Link href="#services">Services</Link></li>
-              <li className='flex items-center justify-center   hover:text-blue-950 hover:scale-110 hover:font-semibold'><Link href="#contact">Contact</Link></li>
-              <li className='flex items-center justify-center   hover:text-blue-950 hover:scale-110 hover:font-semibold'><Link href="#resume">Resume</Link></li>
+              <li className='flex items-center justify-center   hover:text-blue-950 hover:scale-110 hover:font-semibold transition duration-500'><Link href="#intro">Home</Link></li>
+              <li className='flex items-center justify-center   hover:text-blue-950 hover:scale-110 hover:font-semibold transition duration-500'><Link href="#about">About</Link></li>
+              <li className='flex items-center justify-center   hover:text-blue-950 hover:scale-110 hover:font-semibold transition duration-500'><Link href="#services">Services</Link></li>
+              <li className='flex items-center justify-center   hover:text-blue-950 hover:scale-110 hover:font-semibold transition duration-500'><Link href="#contact">Contact</Link></li>
+              <li className='flex items-center justify-center   hover:text-blue-950 hover:scale-110 hover:font-semibold transition duration-500'><Link href="#resume">Resume</Link></li>
 
           </ul>
       </div>
